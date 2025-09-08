8654a369e824139f818a9f6ae34756dc2414ac53
Andrew Fullard <andrewgfullard@gmail.com>
2025-09-08 16:04:22 -0400

    Cleanup begins

--- tardis/plasma/properties/hydrogen_continuum.py
+++ tardis/plasma/properties/hydrogen_continuum.py
@@ -89,28 +89,35 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
         "plus other things",
     )
 
-    def __init__(self, plasma_parent, photo_ion_cross_sections):
+    MAX_ITERATIONS = 1000
+
+    def __init__(
+        self,
+        plasma_parent,
+        photo_ion_cross_sections,
+        atomic_data,
+        ionization_data,
+        nlte_data,
+    ):
         super().__init__(plasma_parent)
         self._update_inputs()
         self.photoionization_data = photo_ion_cross_sections
+        self.atomic_data = atomic_data
+        self.ionization_data = ionization_data
+        self.nlte_data = nlte_data
 
     def calculate_hydrogen_ion_populations(
         self,
-        t_electrons,
-        previous_electron_densities,
+        electron_distribution,
         dilute_planckian_radiation_field,
-        number_density,
+        elemental_number_density,
         lte_level_number_density,
         level_number_density,
         lte_ion_number_density,
         ion_number_density,
+        partition_function,
+        boltzmann_factor,
     ):
-        electron_dist = ThermalElectronEnergyDistribution(
-            0 * u.erg,
-            t_electrons * u.K,
-            previous_electron_densities * u.g / u.cm**3,
-        )
-
         photoionization_rate_solver = AnalyticPhotoionizationRateSolver(
             self.photoionization_data
         )
@@ -127,7 +134,7 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
 
         fractional_ion_population, fractional_electron_density = solver.solve(
             dilute_planckian_radiation_field,
-            electron_dist,
+            electron_distribution,
             elemental_number_density,
             lte_level_number_density,
             level_number_density,
@@ -138,12 +145,12 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
             charge_conservation=False,
         )
 
-        ion_number_density = fractional_ion_population * number_density
-        electron_densities = (
-            fractional_electron_density * previous_electron_densities
+        ion_number_density = (
+            fractional_ion_population * elemental_number_density
         )
+        electron_distribution.number_density *= fractional_electron_density
 
-        return ion_number_density, electron_densities
+        return ion_number_density, electron_distribution
 
     def calculate_lte_quantities(
         self,
@@ -154,7 +161,6 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
         block_ids,
         thermal_g_electron,
         beta_electron,
-        ionization_data,
         thermal_lte_level_boltzmann_factor,
         thermal_lte_partition_function,
     ):
@@ -162,7 +168,7 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
             thermal_g_electron,
             beta_electron,
             thermal_lte_partition_function,
-            ionization_data,
+            self.ionization_data,
         )
 
         lte_ion_number_density = LTEIonNumberDensity(
@@ -184,11 +190,66 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
 
         return lte_ion_number_density, lte_level_number_density
 
+    def calculate_first_guess(
+        self,
+        level_boltzmann_factor_solver,
+        level_number_density_solver,
+        ion_number_density_solver,
+        dilute_planckian_radiation_field,
+        electron_distribution,
+    ):
+        level_boltzmann_factor = (
+            level_boltzmann_factor_solver._calculate_general(
+                self.atomic_data,
+                self.nlte_data,
+                electron_distribution.temperature,  # changes every loop
+                dilute_planckian_radiation_field,
+                None,
+                general_level_boltzmann_factor,  # changes every loop
+                electron_distribution.number_density,  # changes every loop
+                g,
+            )
+        )
+
+        partition_function = level_boltzmann_factor.groupby(
+            level=["atomic_number", "ion_number"]
+        ).sum()
+
+        phi = PhiSahaNebular.calculate(
+            dilute_planckian_radiation_field.temperature,  # changes every loop
+            dilute_planckian_radiation_field.dilution_factor,  # changes every loop
+            zeta_data,
+            electron_distribution.temperature,  # changes every loop
+            delta,  # changes every loop
+            g_electron,
+            beta_rad,
+            partition_function,  # changes every loop
+            self.ionization_data,
+        )
+
+        ion_number_density = ion_number_density_solver.calculate(
+            phi,  # changes every loop
+            partition_function,  # changes every loop
+            number_density,
+        )
+
+        level_number_density = level_number_density_solver.calculate(
+            level_boltzmann_factor,  # changes every loop
+            ion_number_density,  # changes every loop
+            levels,
+            partition_function,  # changes every loop
+        )
+
+        return (
+            level_boltzmann_factor,
+            partition_function,
+            phi,
+            ion_number_density,
+            level_number_density,
+        )
+
     def calculate(
         self,
-        atomic_data,
-        ionization_data,
-        nlte_data,
         levels,
         t_electrons,
         dilute_planckian_radiation_field,
@@ -200,8 +261,6 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
         g,
         thermal_g_electron,
         block_ids,
-        t_rad,
-        w,
         zeta_data,
         delta,
         g_electron,
@@ -218,52 +277,32 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
             self.plasma_parent
         )
 
-        level_boltzmann_factor = (
-            level_boltzmann_factor_solver._calculate_general(
-                atomic_data,
-                nlte_data,
-                t_electrons,  # changes every loop
-                dilute_planckian_radiation_field,
-                None,
-                general_level_boltzmann_factor,  # changes every loop
-                previous_electron_densities,
-                g,
-            )
+        ion_number_density_solver = IonNumberDensity(self.plasma_parent)
+
+        level_number_density_solver = LevelNumberDensity(self.plasma_parent)
+
+        previous_electron_distribution = ThermalElectronEnergyDistribution(
+            0 * u.erg,
+            t_electrons * u.K,
+            previous_electron_densities * u.g / u.cm**3,
         )
 
-        partition_function = level_boltzmann_factor.groupby(
-            level=["atomic_number", "ion_number"]
-        ).sum()
-
-        phi = PhiSahaNebular.calculate(
-            t_rad,
-            w,
-            zeta_data,
-            t_electrons,  # changes every loop
-            delta,  # changes every loop
-            g_electron,
-            beta_rad,
-            partition_function,  # changes every loop
-            ionization_data,
-        )
-
-        ion_number_density = IonNumberDensity(self.plasma_parent).calculate(
-            phi,  # changes every loop
-            partition_function,  # changes every loop
-            number_density,
+        (
+            level_boltzmann_factor,
+            partition_function,
+            phi,
+            ion_number_density,
+            level_number_density,
+        ) = self.calculate_first_guess(
+            level_boltzmann_factor_solver,
+            ion_number_density_solver,
+            level_number_density_solver,
+            dilute_planckian_radiation_field,
+            previous_electron_distribution,
         )
 
         lte_level_number_density_solver = LTELevelNumberDensity(
             self.plasma_parent
-        )
-
-        level_number_density_solver = LevelNumberDensity(self.plasma_parent)
-
-        level_number_density = level_number_density_solver.calculate(
-            level_boltzmann_factor,  # changes every loop
-            ion_number_density,  # changes every loop
-            levels,
-            partition_function,  # changes every loop
         )
 
         lte_level_number_density, lte_ion_number_density = (
@@ -275,32 +314,34 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
                 block_ids,
                 thermal_g_electron,  # changes every loop
                 beta_electron,  # changes every loop
-                ionization_data,
                 thermal_lte_level_boltzmann_factor,  # changes every loop
                 thermal_lte_partition_function,  # changes every loop
             )
         )
 
         level_number_density_converged = False
+        ion_number_density_converged = False
+        thermal_balance_converged = False
 
-        electron_number_density = previous_electron_densities
+        current_electron_distribution = previous_electron_distribution
 
         # Need to solve iteratively to get a consistent solution between
         # hydrogen and other elements. First iteration of the TARDIS simulation
         # uses the analytic solution for the hydrogen ion number density.
         # After that use estimators.
 
-        while not level_number_density_converged:
+        for i in range(self.MAX_ITERATIONS):
             hydrogen_ion_number_density, electron_number_density = (
                 self.calculate_hydrogen_ion_populations(
-                    t_electrons,
-                    previous_electron_densities,
+                    current_electron_distribution,
                     dilute_planckian_radiation_field,
                     number_density,
                     lte_level_number_density,
                     level_number_density,
                     lte_ion_number_density,
                     ion_number_density,
+                    partition_function,
+                    boltzmann_factor,
                 )
             )
 
@@ -309,7 +350,18 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
             ).calculate(phi, partition_function, number_density)
 
             lte_level_number_density, lte_ion_number_density = (
-                self.calculate_lte_quantities(lte_level_number_density_solver)
+                self.calculate_lte_quantities(
+                    lte_level_number_density_solver,
+                    number_density,
+                    previous_electron_densities,
+                    levels,
+                    block_ids,
+                    thermal_g_electron,  # changes every loop
+                    beta_electron,  # changes every loop
+                    ionization_data,
+                    thermal_lte_level_boltzmann_factor,  # changes every loop
+                    thermal_lte_partition_function,  # changes every loop)
+                )
             )
 
             level_number_density = level_number_density_solver.calculate(
@@ -325,11 +377,11 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
                 level_boltzmann_factor_solver._calculate_general(
                     atomic_data,
                     nlte_data,
-                    t_electrons,
+                    t_electrons,  # changes every loop
                     dilute_planckian_radiation_field,
                     None,
-                    general_level_boltzmann_factor,
-                    electron_number_density,
+                    general_level_boltzmann_factor,  # changes every loop
+                    electron_number_density,  # changes every loop
                     g,
                 )
             )
@@ -338,7 +390,9 @@ class HydrogenContinuumProperties(ProcessingPlasmaProperty):
                 level=["atomic_number", "ion_number"]
             ).sum()
 
-            level_number_density_converged = True
+            level_number_density_converged = False
+            ion_number_density_converged = False
+            thermal_balance_converged = False
 
         stimulated_emission_factor = (
             self.plasma_parent.stimulated_emission_factor
